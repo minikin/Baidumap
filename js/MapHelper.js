00a3e5638184bc9ee59fb6a425f1f331612d3151
minikin <djminikin@gmail.com>
2017-10-24 12:06:58 +0200

    Update implementation of MapHelper.js

--- js/MapHelper.js
+++ js/MapHelper.js
@@ -1,17 +1,18 @@
 import { NativeModules, } from 'react-native';
 
 const module = NativeModules.BaiduBridgeHelper;
-export default class MapHelper {
 
+export default  {
+    
     setPinImageWith(imagePath){
         module.setPinImageWith(imagePath)
-    }
+    },
 
     setCustomMapStyle(enable){
         module.customMapStyle(enable)
-    }
+    },
 
     setCustomMapWithJSON(stylePath) {
         module.setCustomMapStyleWith(stylePath)
     }
-}
+};
